9bf5d492f3723c3a748773cc3b186161ee1310fa
Ahmeis <ahmedmheissa@gmail.com>
2016-09-15 19:36:25 +0200

    Removed column offsets

--- Location.java
+++ Location.java
@@ -1,29 +1,19 @@
 
 public class Location {
 	private int xPos, yPos; // x Position, y Position
-	private int xCC, yCT; // x Column Center, y Column Top
 	private String choice; // choice
 	private boolean type; // True for ID false for choice
 	private int number; // Question number/Digit number in ID
 	private int page=1; // Page number
 	private int xRef, yRef; // Reference point
-	Location(int X, int Y){
+	Location(int X, int Y, int pageNumber){
 		xRef = X;
 		yRef = Y;
-	}
-	Location(Location Ref,boolean ID, int xColumnCenter, int yColumnTop, int pageNumber){
-		xRef = Ref.getXRef();
-		yRef = Ref.getYRef();
-		xCC = xColumnCenter;
-		yCT = yColumnTop;
 		page = pageNumber;
-		setType(ID);
 	}
 	Location(Location Ref, int X, int Y, String Choice, int Number){
 		xRef = Ref.getXRef();
 		yRef = Ref.getYRef();
-		xCC =  Ref.getColumnCenter();
-		yCT = Ref.getColumnTop();
 		page = Ref.getPageNumber();
 		type = Ref.getType();
 		xPos = X;
@@ -38,12 +28,6 @@ public class Location {
 	boolean getType(){
 		return type;
 	}
-	int getColumnCenter(){
-		return xCC;
-	}
-	int getColumnTop(){
-		return yCT;
-	}
 	void setPageNumber(int P){
 		page = P;
 	}
@@ -56,10 +40,6 @@ public class Location {
 	void setLocation(int X, int Y){
 		xPos = X;
 		yPos = Y;
-	}
-	void setOffset(int X, int Y){
-		xCC = X;
-		yCT = Y;
 	}
 	void setType(boolean ID){
 		type = ID;
@@ -80,8 +60,8 @@ public class Location {
 		S+=number + " ";
 		S+= choice + " ";
 		int x,y;
-		x = xPos + xCC - xRef;
-		y = yPos + yCT - yRef;
+		x = xPos - xRef;
+		y = yPos - yRef;
 		S+= x + " ";
 		S+= y + " ";
 		S+= page;
